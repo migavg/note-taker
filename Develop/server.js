9ca081159eb482442615bdab8eeeeef5d67f1ad7
mvill <119754846+migavg@users.noreply.github.com>
2023-03-15 21:02:18 -0700

    added some app.get functions to see if page can be opened

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,21 @@ const path = require('path');
 const api = require("./public/assets/js/index");
 const PORT = 3001;
 const app = express();
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+app.get('/index', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
